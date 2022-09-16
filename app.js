46daf6d5d3a7a58b8925c06d6bd4fe75cede3358
josesanchez999 <josejsanchez22@icloud.com>
2022-09-16 12:41:13 -0500

    changes

---
+++ app.js
@@ -0,0 +1,31 @@
+let game = 'Minecraft';
+console.log('My favorite game is  ' + game + '.'); 
+// My favorite game is Minecraft.
+
+const blocks = 20; //Blocks is for the Minecraft Theme.
+if(blocks > 16){
+    console.log('You have 20 blocks of grass!')
+}
+switch ("no diamonds") {
+    case "iron":
+      console.log("I have iron");
+      break;
+    case "gold":
+      console.log("Ihave gold");
+      break;
+    case "emeralds":
+      console.log("I have emeralds");
+      break;
+    default:
+      console.log("I don't have diamonds. Sorry");
+      break;
+  } //Minecraft ores in Switch codes
+
+  const name = "Steve";
+  const iron = 32;
+  const combined = name + iron;
+  console.log(combined); // (String Method) Steve is the minecraft characher and iron number represents a half of stack of iron.
+
+  let x = '11'
+  let num = parseFloat(x)
+  console.log(num) //Number Method 11 Gold ingots
